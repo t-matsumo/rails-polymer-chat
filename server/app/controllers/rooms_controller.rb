1a924e9db9eee8c4757e9de9ded0ae5d1f16e7d6
t-matsumo <tatsukimatsumo@gmail.com>
2018-04-01 16:30:14 +0900

    Add rooms CRUD API

--- server/app/controllers/rooms_controller.rb
+++ server/app/controllers/rooms_controller.rb
@@ -1,2 +1,33 @@
 class RoomsController < ApplicationController
+  def index
+    render json: Room.all
+  end
+
+  def show
+    render json: Room.find(params[:id])
+  end
+
+  def create
+    room = Room.new(room_params)
+    room.save!
+    render json: room
+    end
+  end
+
+  def update
+    room = Room.find(params[:id])
+    room.update!(article_params)
+    render :json room
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    render :json room
+  end
+
+  private
+    def room_params
+      params.require(:room).permit(:name)
+    end
 end
