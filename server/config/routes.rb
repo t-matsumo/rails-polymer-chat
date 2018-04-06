15837fbc714d771002112373456d143e8e4477af
t-matsumo <tatsukimatsumo@gmail.com>
2018-04-06 22:22:41 +0900

    Change route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :rooms, only: [:index, :show, :create, :update, :destroy]
+  resources :rooms, only: [:index, :show, :create, :update, :destroy], shallow: true do
+    resources :messages, only: [:index, :show, :create, :update, :destroy]
+  end
   
   mount ActionCable.server => '/cable'
 end
