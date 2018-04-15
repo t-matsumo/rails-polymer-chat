36b42bb180c6dda6d065b060b1111d98859effd6
t-matsumo <tatsukimatsumo@gmail.com>
2018-04-15 13:52:06 +0900

    Relate room to messages

--- server/db/migrate/20180406124911_create_messages.rb
+++ server/db/migrate/20180406124911_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
       t.string :body
+      t.belongs_to :room, index: true
 
       t.timestamps
     end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401050725) do
+ActiveRecord::Schema.define(version: 20180406124911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.bigint "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
