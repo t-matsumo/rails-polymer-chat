83d021fca883fbbcd6eb0e5da062c8750f461473
t-matsumo <tatsukimatsumo@gmail.com>
2018-04-15 14:06:05 +0900

    Room chat

--- server/app/channels/chat_channel.rb
+++ server/app/channels/chat_channel.rb
@@ -1,6 +1,6 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    room = Room.find(params[:id])
+    room = Room.find params[:id]
     stream_for room
   end
 
@@ -9,7 +9,8 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def speak(message)
-    room = Room.find(params[:id])
+    room = Room.find params[:id]
+    room.messages.create!({body: message["body"]})
     ChatChannel.broadcast_to(room, message)
   end
 end
