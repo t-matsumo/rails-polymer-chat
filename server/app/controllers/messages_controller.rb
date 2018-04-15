927d7651dbb73f7e6d4ded7a370a9a61c4c74cfd
t-matsumo <tatsukimatsumo@gmail.com>
2018-04-15 13:52:26 +0900

    Implements message controller

--- server/app/controllers/messages_controller.rb
+++ server/app/controllers/messages_controller.rb
@@ -1,2 +1,30 @@
 class MessagesController < ApplicationController
+  def index
+    room = Room.find params[:room_id]
+    render json: room.messages
+  end
+
+  def show
+    render json: Message.find(params[:id])
+  end
+
+  def create
+    room = Room.find params[:room_id]
+    render json: room.messages.create!(message_params)
+  end
+
+  def update
+    message = Message.find params[:id]
+    message.update! message_params
+  end
+
+  def destroy
+    message = Message.find params[:id]
+    message.destroy
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:body)
+    end
 end
